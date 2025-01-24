a167321f85c4dbf257723de5d9cb2f8bd3f2d039
embruna <embruna@ufl.edu>
2025-01-24 17:04:18 -0500

    editing data import

--- corridor_docs/eric_ms_thesis/01_cleanup_sample_info.R
+++ corridor_docs/eric_ms_thesis/01_cleanup_sample_info.R
@@ -4,6 +4,7 @@
 # load libraries ----------------------------------------------------------
 
 library(tidyverse)
+library(here)
 library(lubridate) # to deal with dates https://lubridate.tidyverse.org/
 # library(here)
 # load data ---------------------------------------------------------------
@@ -63,3 +64,98 @@ write_csv(btl,"./corridor_docs/eric_ms_thesis/ms_data/data_clean/clean_sample_in
 # 
 # btl<-read_csv(here("corridor_docs","eric_ms_thesis","ms_data","data_raw","samples.csv"))
 #   
+
+# load beetle id data -----------------------------------------------------
+library(vegan)
+library(ggvegan)
+# vegan cheatsheet: https://www.rpubs.com/an-bui/vegan-cheat-sheet
+
+
+
+env <- read_csv(here("corridor_docs","eric_ms_thesis","ms_data","data_raw","samples.csv")) %>% 
+  mutate(sample_id=as.factor(sample_id)) 
+
+btl_counts<-read_csv(here("corridor_docs","eric_ms_thesis","ms_data","data_raw","beetle_counts.csv")) %>% 
+  mutate(sample_id=as.factor(sample_id)) %>% 
+  rowwise() %>%
+  mutate(sum = sum(c_across(pvin:ocon), na.rm = TRUE)) %>% 
+  mutate(sum=ifelse(sum==0,NA,sum)) %>% 
+  replace(is.na(.), 0) %>% 
+  left_join(env) %>% 
+  relocate(c(block,patch, point, month, day),.after = sample_id)
+
+btl_summary<-btl_counts %>% 
+  select(-c(sample_id,block,point,month,day)) %>% 
+  group_by(patch) %>% 
+  summarize(across(everything(), ~ sum(.x, na.rm = TRUE))) %>% 
+  filter(!is.na(patch)) %>% 
+  select(-sum) %>% 
+  column_to_rownames("patch")
+
+
+  
+  summarize(sum=sum(c_across(pvin:ocon), na.rm = TRUE)) %>% 
+  filter(!is.na(patch))
+btls<-btl_summary %>% 
+  
+
+
+
+
+btls<-btl_counts %>% 
+  select(-c(sum,sample_id,block,point, month, day)) %>% 
+  # column_to_rownames("sample_id")
+  column_to_rownames("patch")
+
+
+
+
+
+
+
+site_type <- env %>% 
+  select(block, patch)
+# How speciose are my communities?
+  
+# specnumber() will tell you the number of species within each sample. 
+# You can then run an analysis of variance to ask if mean species richness 
+# is significantly different across sites.
+
+sppr <- specnumber(btl_summary)
+
+# analysis of variance takes the same form as the usual models you'd see in R
+# response ~ dependent, data = environmental grouping
+# sppr_aov <- aov(sppr ~ patch, data = site_type)
+# summary(sppr_aov)
+
+shannondiv <- diversity(btl_summary)
+head(shannondiv)
+
+# rarefaction
+
+
+
+btl_summary<-btl_counts %>% 
+  select(-c(patch,block,point,month,day)) %>% 
+  # select(-c(sample_id,block,point,month,day)) %>% 
+  # group_by(patch) %>% 
+  filter(sum>0) %>% 
+  select(-sum) %>% 
+  column_to_rownames("sample_id")
+
+
+
+spAbund <- rowSums(btl_summary)  #gives the number of individuals found in each plot
+spAbund # view observations per plot 
+# ufl graduation deadlines graduate school
+
+#rarefaction uses the smallest number of 
+# observations per sample to extrapolate the expected number if all other 
+# samples only had that number of observations
+raremin <- min(rowSums(btl_summary))  
+raremin # view smallest # of obs 
+
+
+sRare <- rarefy(btl_summary, raremin) # now use function rarefy
+sRare #give
+rarecurve(btl_summary, col = "blue") 
