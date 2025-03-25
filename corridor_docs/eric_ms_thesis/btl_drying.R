d5ae9c92d5fb558c48bb721575e2228779f9cb1b
embruna <embruna@ufl.edu>
2025-03-25 15:01:06 -0400

    updated beetle wts

---
+++ corridor_docs/eric_ms_thesis/btl_drying.R
@@ -0,0 +1,29 @@
+# Script to determine if weights of btls in drying overn have stabilized  
+# date: 19 March 2025
+
+# load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(here)
+
+
+# load data ---------------------------------------------------------------
+
+btl_wt<-read_csv(here("corridor_docs","eric_ms_thesis","ms_data","data_raw","btl_weight_equilibrium.csv"))
+
+btl_wt<-btl_wt %>% 
+  pivot_longer(wet_weight:weight_4,names_to = "interval") %>% 
+  mutate(interval=if_else(interval=="wet_weight","weight_0",interval)) %>% 
+  arrange(sample_id,sp_code,interval) %>% 
+  group_by(sample_id,sp_code) %>% 
+  mutate(perc_loss=((lag(value)-(value))/lag(value)*100)) %>% 
+  mutate(sample=paste(sp_code,sample_id,sep="-"))
+  
+btl_wt
+
+
+btl_wt %>% 
+  # filter(interval!="weight_1") %>%
+  # filter(interval!="weight_0") %>%
+  ggplot( aes(x=interval, y=value, group=sample, color=sample)) +
+  geom_line()
